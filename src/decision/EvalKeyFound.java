432e1074831accce404f6d191a036f27324bb617
apurohi <apurohi@ncsu.edu>
2016-04-03 14:35:01 -0400

    Added the code for 2 decision evaluations - to check whether key has been found or not, and to check if there is a variation in the signal strength among neighbouring edges.

--- src/decision/EvalKeyFound.java
+++ src/decision/EvalKeyFound.java
@@ -1,14 +1,19 @@
 package decision;
 
+import data.BombermanMap;
+import data.Const;
+
 import java.util.HashMap;
 
 /**
  * Evaluates whether or not the key / treasure has been found
+ *
  * Created by Anand on 4/2/2016.
  */
 public class EvalKeyFound extends Evaluation {
     @Override
     public DTreeNode evaluate(HashMap<Integer, Object> paramMap) {
-        return null;
+        BombermanMap map = (BombermanMap) paramMap.get(Const.DecisionTreeParams.GRAPH_KEY);
+        return map.isTreaureVisible() ? children.get(TRUE_INDEX) : children.get(FALSE_INDEX);
     }
 }
--- src/decision/EvalSignalStrVariation.java
+++ src/decision/EvalSignalStrVariation.java
@@ -1,15 +1,36 @@
 package decision;
 
+import data.BombermanMap;
+import data.Const;
+import data.PlayerInfo;
+import data.Tile;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
  * Evaluates whether or not there is a variation in signal strength among adjacent tiles
- *
+ * <p/>
  * Created by Anand on 4/2/2016.
  */
 public class EvalSignalStrVariation extends Evaluation {
     @Override
     public DTreeNode evaluate(HashMap<Integer, Object> paramMap) {
-        return null;
+        PlayerInfo player = (PlayerInfo) paramMap.get(Const.DecisionTreeParams.CURR_CHAR_KEY);
+        BombermanMap map = (BombermanMap) paramMap.get(Const.DecisionTreeParams.GRAPH_KEY);
+        Tile currTile = (Tile) paramMap.get(Const.DecisionTreeParams.CURR_TILE_KEY);
+
+        boolean flag = false;
+        ArrayList<String> adjList = map.edges.get(currTile.toString());
+        for (int i = 0; i < adjList.size(); i += 2) {
+            String adjTileStr = adjList.get(i);
+            Tile adjTile = Tile.toTile(adjTileStr, map);
+            if (adjTile.signal != currTile.signal) {
+                flag = true;
+                break;
+            }
+        }
+
+        return flag ? children.get(TRUE_INDEX) : children.get(FALSE_INDEX);
     }
 }

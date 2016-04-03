5724581487d9cb5e2eb839e0a2fe324853c6993d
apurohi <apurohi@ncsu.edu>
2016-04-02 21:36:11 -0400

    DTreeNode : Added constants through which various objects can be accessed in the paramsMap (used in decision evaluation, and in performing action)

--- src/decision/DTreeNode.java
+++ src/decision/DTreeNode.java
@@ -4,4 +4,8 @@ package decision;
  * Created by Anand on 4/2/2016.
  */
 public class DTreeNode {
+    public static final int GRAPH_KEY = 1;
+    public static final int PLAYER_KEY = 2;
+    public static final int ENEMY_KEY = 3;
+    public static final int CURR_TILE_KEY = 4;
 }
